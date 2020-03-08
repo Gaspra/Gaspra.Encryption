ce00b50f5586b18435db3b549c8338dc5bcad541
Richard Lawrence <richardlaw192@gmail.com>
2020-03-08 17:07:37 +0000

    Adding project

---
+++ src/Gaspra.Encryption/Gaspra.Encryption/Program.cs
@@ -0,0 +1,37 @@
+﻿using Gaspra.Signing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace Gaspra.Encryption
+{
+    class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args)
+                .RunConsoleAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((host, config) =>
+            {
+                host
+                    .HostingEnvironment.EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                config
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile($"appsettings.{host.HostingEnvironment.EnvironmentName}.json");
+            })
+            .ConfigureServices((host, services) =>
+            {
+                services
+                    .RegisterSigningServices()
+                    .AddHostedService<EncryptionService>();
+            });
+    }
+}
